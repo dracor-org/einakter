caefb19eb0bb4a9e3ffabd7ac60b14e536b49bc4
Carsten Milling <carsten.milling@bluereport.net>
2021-03-03 01:54:04 +0100

    Fetch only new locations

--- fetch-locations.ts
+++ fetch-locations.ts
@@ -3,6 +3,10 @@ import {loadAll, CORE_SCHEMA} from 'js-yaml';
 import {readFileSync, writeFileSync} from 'fs';
 import {Play} from './src/types';
 
+import loc from './src/locations.json';
+
+const locations: {[index: string]: any} = {...loc}
+
 let data: Play[] = [];
 try {
   data = loadAll(readFileSync('./data.yaml', 'utf8'), null, {
@@ -21,34 +25,39 @@ const endpoint = 'https://query.wikidata.org/sparql';
 
 async function fetchLocations () {
   const results: {[index: string]: any} = {};
-
   for(let i = 0; i < locationIds.length; i++) {
     const id = locationIds[i];
-    const sparql = `SELECT ?loc WHERE { wd:${id} wdt:P625 ?loc .}`;
-    const url = `${endpoint}?query=${encodeURIComponent(sparql)}`;
-    console.log(`${id} ${url}`);
+    const loc = locations[id as string];
+    if (loc) {
+      console.log(`${id} exists`);
+      results[id as string] = loc;
+    } else {
+      const sparql = `SELECT ?loc WHERE { wd:${id} wdt:P625 ?loc .}`;
+      const url = `${endpoint}?query=${encodeURIComponent(sparql)}`;
+      console.log(`${id} ${url}`);
 
-    try {
-      const response = await axios.get(url);
-      if (response.status === 200) {
-        const loc = response.data.results?.bindings[0]?.loc.value;
-        let long: number, lat: number;
-        const m = loc.match(/\((-?[.0-9]+) (-?[.0-9]+)\)/);
-        if (m) {
-          long = parseFloat(m[1]);
-          lat = parseFloat(m[2]);
-          results[id as string] = [lat, long]
-          console.log(loc, long, lat);
+      try {
+        const response = await axios.get(url);
+        if (response.status === 200) {
+          const loc = response.data.results?.bindings[0]?.loc.value;
+          let long: number, lat: number;
+          const m = loc.match(/\((-?[.0-9]+) (-?[.0-9]+)\)/);
+          if (m) {
+            long = parseFloat(m[1]);
+            lat = parseFloat(m[2]);
+            results[id as string] = [lat, long]
+            console.log(loc, long, lat);
+          }
+        } else {
+          console.log(response.status);
         }
-      } else {
-        console.log(response.status);
+      } catch (error) {
+        console.log(error)
       }
-    } catch (error) {
-      console.log(error)
+
+      console.log('waiting...');
+      await new Promise(r => setTimeout(r, 1000));
     }
-    
-    console.log('waiting...');
-    await new Promise(r => setTimeout(r, 1000));
   }
 
   console.log(results);
