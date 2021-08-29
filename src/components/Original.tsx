d7722a46636fccea2d6193def21f917f74864db4
Carsten Milling <cmil@hashtable.de>
2021-08-30 00:37:58 +0200

    Add list of other translations

--- src/components/Original.tsx
+++ src/components/Original.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Link, useParams} from "react-router-dom";
 import IdLink from './IdLink';
+import {EinakterContext} from '../context';
 import {OriginalPlay} from '../types';
 
 function getYear(ref: OriginalPlay) {
@@ -24,7 +26,11 @@ interface Props {
 };
 
 const Original = ({data}: Props) => {
+  const {id: currentId} = useParams<{id: string}>();
+  const {plays} = useContext(EinakterContext);
+
   const {
+    id,
     author,
     title,
     subtitle,
@@ -35,6 +41,10 @@ const Original = ({data}: Props) => {
 
   const year = getYear(data);
 
+  const others = plays.filter((p) => p.basedOn?.find(
+    (r: OriginalPlay) => r.id === id && p.slug !== currentId
+  ));
+  
   return (
     <>
       {author?.name}
@@ -72,6 +82,26 @@ const Original = ({data}: Props) => {
       )}
       {year !== undefined && ` (${year})`}
       {language && ` <${languages[language] || language}>`}
+
+      {others.length > 0 && (
+        <>
+          <p style={{margin: '.8em 0 .3em', fontStyle: 'italic'}}>
+            Other one-act translations:
+          </p>
+          <ul>
+            {others.map((play) => (
+              <li key={play.slug}>
+                <Link to={`/${play.slug}`}>
+                  {play.author?.name && `${play.author.name}: `}
+                  {play.title}
+                  {play.subtitle && `. ${play.subtitle}`}
+                  {play.normalizedYear && ` (${play.normalizedYear})`}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 };
