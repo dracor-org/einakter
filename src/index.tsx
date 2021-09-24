d43ad75258dc2e870d2e9a227bd708ebc3ec4b0c
Carsten Milling <cmil@hashtable.de>
2021-09-25 00:43:54 +0200

    Remove bootstrap and (unused) react-router-bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { I18nProvider } from '@lingui/react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import i18n from './i18n';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
