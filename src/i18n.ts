26f4c72180b2aa3faa22b42a2b085a76ed05b6e9
Carsten Milling <cmil@hashtable.de>
2021-10-23 22:36:35 +0200

    Remove Turkish translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,22 +1,19 @@
 import { i18n } from '@lingui/core';
 import { detect, fromStorage } from '@lingui/detect-locale';
-import { en, de, tr } from 'make-plural/plurals';
+import { en, de } from 'make-plural/plurals';
 import { messages as enMessages } from './locales/en/messages';
 import { messages as deMessages } from './locales/de/messages';
-import { messages as trMessages } from './locales/tr/messages';
 
 const storageKey = 'uiLanguage';
 
 i18n.loadLocaleData({
   en: { plurals: en },
   de: { plurals: de },
-  tr: { plurals: tr },
 });
 
 i18n.load({
   en: enMessages,
   de: deMessages,
-  tr: trMessages,
 });
 
 const language = detect(
@@ -26,7 +23,7 @@ const language = detect(
 
 i18n.activate(language);
 
-export const locales = ['en', 'de', 'tr'];
+export const locales = ['en', 'de'];
 
 export function setLocale (locale: string) {
   i18n.activate(locale);
