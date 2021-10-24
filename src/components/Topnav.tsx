5aabdc1cd4fecc3c469df055485db1e72a9907e6
Carsten Milling <cmil@hashtable.de>
2021-10-25 01:38:45 +0200

    Refactor TopNav to remove react-bootstrap components

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import React, {ReactNode, useState} from 'react';
+import {Link} from 'react-router-dom';
+import {classnames, TTailwindString} from 'tailwindcss-classnames';
 import {useLocation} from 'react-router-dom';
 import {useLingui} from "@lingui/react";
 import {Trans} from '@lingui/macro';
@@ -8,7 +8,30 @@ import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {locales, setLocale} from '../i18n';
 
+interface NavItemProps {
+  href: string;
+  children: ReactNode;
+  className?: TTailwindString;
+}
+
+function NavItem ({href, className, children}: NavItemProps) {
+  const location = useLocation();
+
+  const isActive = location.pathname.startsWith(href);
+
+  const classes = classnames(
+    'block', 'mt-4', 'md:inline-block', 'md:mt-0', 'text-blue-100',
+    'hover:text-white', 'mr-4', className,
+    {italic: isActive}
+  );
+
+  return (
+    <Link to={href} className={classes}>{children}</Link>
+  );
+}
+
 function Topnav () {
+  const [showNav, setShowNav] = useState(false);
   const location = useLocation();
   const {i18n} = useLingui();
 
@@ -17,61 +40,70 @@ function Topnav () {
   }
 
   return (
-    <Navbar className="flex p-4 bg-primary text-white uppercase font-medium" bg="dark" variant="dark" expand="lg" sticky="top">
-      <Navbar.Brand href="/" className="z-0">
-        <img
-          src="/DONE.svg"
-          width="170"
-          alt="Einakter"
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" className="flex flex-grow -ml-28">
-        <Nav className="flex m-auto gap-8">
-          <Nav.Link
-            href="/about"
-            active={isActive('/about')}
-            className="text-white"
-          >
+    <nav className="flex items-center justify-between flex-wrap p-4 bg-primary text-white font-medium">
+      <div className="flex items-center flex-shrink-0 text-white">
+        <a href="/" title="Einakter">
+          <img src="/DONE.svg" width="170" alt="Einakter" />
+        </a>
+      </div>
+      <div className="block md:hidden">
+        <button
+          className={classnames(
+            'flex', 'items-center', 'px-3', 'py-2', 'border', 'rounded',
+            'hover:text-white',  'hover:border-white'
+          )}
+          onClick={() => setShowNav(!showNav)}
+        >
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div className={classnames(
+        'uppercase', 'w-full', 'block', 'flex-grow', 'md:flex',
+        'md:items-center', 'md:w-auto', 'md:ml-5',
+        {hidden: !showNav}
+      )}>
+        <div className="mb-3 md:flex-grow md:mb-0">
+          <NavItem href="/about">
             <Trans>About</Trans>
-          </Nav.Link>
-          <Nav.Link
-            href="/locations"
-            active={isActive('/locations')}
-            className="text-white"
-          >
+          </NavItem>
+          <NavItem href="/locations">
             <Trans>Locations</Trans>
-          </Nav.Link>
-          <Nav.Link
-            href="/originals"
-            active={isActive('/originals')}
-            className="text-white"
-          >
+          </NavItem>
+          <NavItem href="/originals">
             <Trans>Originals</Trans>
-          </Nav.Link>
-        </Nav>
-        <Nav>
+          </NavItem>
+        </div>
+        <div className="mr-2 mb-3 md:mb-0">
           {locales.map((locale) => (
-            <span
+            <button
               key={locale}
-              style={{cursor: 'pointer'}}
-              className={`nav-link${locale === i18n.locale ? ' active' : ''}`}
+              className={classnames(
+                'text-blue-100', 'mr-3', 'hover:text-white', {
+                  'font-bold': locale === i18n.locale
+                }
+              )}
               onClick={() => setLocale(locale)}
             >
               {locale}
-            </span>
+            </button>
           ))}
-          <Nav.Link
+        </div>
+        <div>
+          <a
             href="https://github.com/dracor-org/einakter"
             title="Einakter Github"
             className="text-white"
           >
-            <FontAwesomeIcon className="scale-0 sm:scale-100" icon={faGithub} size="3x"/>
+            <FontAwesomeIcon
+              className="scale-0 sm:scale-100"
+              icon={faGithub}
+              size="3x"
+            />
             <span className="block sm:hidden">Einakter Github</span>
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 }
 
