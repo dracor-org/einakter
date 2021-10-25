975e9ce4e9797908ae33258050cf76523b2ad2e9
lehkost <frafis@gmail.com>
2021-10-25 15:02:39 +0200

    add Spanish localisation (thx to Pablo Ruiz Fabo)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,22 @@
 import { i18n } from '@lingui/core';
 import { detect, fromStorage } from '@lingui/detect-locale';
-import { en, de } from 'make-plural/plurals';
+import { en, de, es } from 'make-plural/plurals';
 import { messages as enMessages } from './locales/en/messages';
 import { messages as deMessages } from './locales/de/messages';
+import { messages as esMessages } from './locales/es/messages';
 
 const storageKey = 'uiLanguage';
 
 i18n.loadLocaleData({
   en: { plurals: en },
   de: { plurals: de },
+  es: { plurals: es },
 });
 
 i18n.load({
   en: enMessages,
   de: deMessages,
+  es: esMessages,
 });
 
 const language = detect(
@@ -23,7 +26,7 @@ const language = detect(
 
 i18n.activate(language);
 
-export const locales = ['en', 'de'];
+export const locales = ['en', 'de', 'es'];
 
 export function setLocale (locale: string) {
   i18n.activate(locale);
