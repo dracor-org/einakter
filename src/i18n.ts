87e898b23e731955cc65ad6ffed65e8df7f0dc8b
lehkost <frafis@gmail.com>
2021-10-26 16:29:54 +0200

    add Russian localisation (thx to Daniil Skorinkin)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,10 @@
 import { i18n } from '@lingui/core';
 import { detect, fromStorage } from '@lingui/detect-locale';
-import { en, de, es } from 'make-plural/plurals';
+import { en, de, es, ru } from 'make-plural/plurals';
 import { messages as enMessages } from './locales/en/messages';
 import { messages as deMessages } from './locales/de/messages';
 import { messages as esMessages } from './locales/es/messages';
+import { messages as ruMessages } from './locales/ru/messages';
 
 const storageKey = 'uiLanguage';
 
@@ -11,12 +12,14 @@ i18n.loadLocaleData({
   en: { plurals: en },
   de: { plurals: de },
   es: { plurals: es },
+  ru: { plurals: ru },
 });
 
 i18n.load({
   en: enMessages,
   de: deMessages,
   es: esMessages,
+  ru: ruMessages,
 });
 
 const language = detect(
@@ -26,7 +29,7 @@ const language = detect(
 
 i18n.activate(language);
 
-export const locales = ['en', 'de', 'es'];
+export const locales = ['en', 'de', 'es', 'ru'];
 
 export function setLocale (locale: string) {
   i18n.activate(locale);
