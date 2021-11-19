4c8cbadd10eebc126017ebde29559bfd37a72acc
Mark Schwindt <mark.schwindt@rub.de>
2021-11-19 14:28:16 +0100

    Apply default typography to markdown

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const Content = lazy(() => importMDX('../about.mdx'));
 
 export default function About () {
   return (
-    <div className="page p-4">
+    <div className="page p-4 markdown max-w-screen-lg">
       <Helmet>
         <title>Einakter: {t`About`}</title>
       </Helmet>
