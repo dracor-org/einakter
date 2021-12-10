b2b14f5ba72cfcdeecc2c735931249cce567ecad
lehkost <frafis@gmail.com>
2021-12-10 10:33:26 +0100

    add Japanese localisation

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,11 @@
 import { i18n } from '@lingui/core';
 import { detect, fromStorage } from '@lingui/detect-locale';
-import { en, de, es, fr, ko, ru, zh } from 'make-plural/plurals';
+import { en, de, es, fr, ja, ko, ru, zh } from 'make-plural/plurals';
 import { messages as enMessages } from './locales/en/messages';
 import { messages as deMessages } from './locales/de/messages';
 import { messages as esMessages } from './locales/es/messages';
 import { messages as frMessages } from './locales/fr/messages';
+import { messages as jaMessages } from './locales/ja/messages';
 import { messages as koMessages } from './locales/ko/messages';
 import { messages as ruMessages } from './locales/ru/messages';
 import { messages as zhMessages } from './locales/zh/messages';
@@ -16,6 +17,7 @@ i18n.loadLocaleData({
   de: { plurals: de },
   es: { plurals: es },
   fr: { plurals: fr },
+  ja: { plurals: ja },
   ko: { plurals: ko },
   ru: { plurals: ru },
   zh: { plurals: zh },
@@ -26,6 +28,7 @@ i18n.load({
   de: deMessages,
   es: esMessages,
   fr: frMessages,
+  ja: jaMessages,
   ko: koMessages,
   ru: ruMessages,
   zh: zhMessages,
@@ -38,7 +41,7 @@ const language = detect(
 
 i18n.activate(language);
 
-export const locales = ['en', 'de', 'es', 'fr', 'ko', 'ru', 'zh'];
+export const locales = ['en', 'de', 'es', 'fr', 'ja', 'ko', 'ru', 'zh'];
 
 export function setLocale (locale: string) {
   i18n.activate(locale);
