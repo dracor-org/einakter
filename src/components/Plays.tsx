ff8e3ba81a4ed146d4fc762d28654a5125cce0d7
Carsten Milling <cmil@hashtable.de>
2022-09-24 15:29:40 +0200

    Replace react-bootstrap-table with @tanstack/react-table

--- src/components/Plays.tsx
+++ src/components/Plays.tsx
@@ -1,149 +1,104 @@
-import React from 'react';
-import BootstrapTable from 'react-bootstrap-table-next';
-import ToolkitProvider, {Search} from 'react-bootstrap-table2-toolkit';
-import {Link} from 'react-router-dom';
+import React, {useMemo} from 'react';
 import {t} from '@lingui/macro';
 import {Helmet} from 'react-helmet-async';
-import Authors from './Authors';
-import Statistics from './Statistics';
-import IdLink from './IdLink';
+import {ColumnDef} from '@tanstack/react-table';
 import {Play} from '../types';
 import data from '../data.json';
 import authors from '../authors.json';
+import Statistics from './Statistics';
+import Table from './Table';
+import Authors from './Authors';
+import TitleCell from './TitleCell';
 
-function formatAuthor (_: string, play: Play) {
-  return <Authors authors={play.authors || []} withLink/>;
-}
-
-function formatTitle (_: string, play: Play) {
-  const {title, subtitle, slug, ids: {wikidata, dracor} = {}} = play;
+function KeywordsCell ({keywords}: {keywords: string[]}) {
   return (
-    <span>
-      <Link  className="text-lg"  to={`/${slug}`}>{title}</Link>
-      {subtitle && <small><br/>{subtitle}</small>}
-      <div>
-      {wikidata && (
-        <small>
-          <IdLink id={wikidata} type="wikidata"/>
-        </small>
-      )}
-      {' '}
-      {dracor && (
-        <small>
-          <IdLink id={dracor} type="dracor"/>
-        </small>
-      )}
-      </div>
-    </span>
-  );
-}
-
-function formatKeywords (_: string, play: Play) {
-  const {keywords} = play;
-  return keywords ? (
     <ul className="list-disc text-gray-400 text-sm">
       {keywords.map(text => (
         <li key={text}><span className="text-black">{text}</span></li>
       ))}
     </ul>
-  ) : <span/>;
+  );
 }
 
-function Table () {
-  const columns = [{
-    dataField: 'authors[0].name',
-    text: t`Author`,
-    formatter: formatAuthor,
-    filterValue: (cell: string | undefined, play: Play) => {
-      if (cell === undefined) return 'Anonym';
-      const {authors = []} = play;
-      return authors.map(a => `${a.name || ''} ${a.pseudonym || ''} ${a.wikidata || ''}`).join(' ');
-    },
-    sort: true
-  }, {
-    dataField: 'title',
-    text: t`Title`,
-    formatter: formatTitle,
-    filterValue: (_: string, play: Play) => {
-      const settings = play.settings?.map((s) => s.description).join(' ');
-      let text = `${play.title} ${play.subtitle} ${settings}`;
-      play.keywords?.forEach(k => text += ` ${k}`);
-      play.cast?.forEach(c => text += ` ${c.name}`)
-      return text;
-    },
-    sort: true
-  }, {
-    dataField: 'normalizedYear',
-    text: t`Year (normalized)`,
-    sort: true,
-    sortFunc: (a: number, b: number, order: string) => {
-      if (!a) {
-        return order === 'asc' ? -1 : 1;
-      }
+export default function Plays () {
+  const columns = useMemo<ColumnDef<Play>[]>(
+    () => [
+      {
+        id: 'authors',
+        header: t`Author`,
+        accessorFn: row => {
+          const {authors = []} = row;
+          if (authors.length === 0) return 'Anonym';
+          return authors.map(
+            a => `${a.name || ''} ${a.pseudonym || ''} ${a.wikidata || ''}`
+          ).join(' ');
+        },
+        cell: info => (
+          <Authors authors={info.row.original.authors || []} withLink/>
+        ),
+      },
+      {
+        accessorKey: 'title',
+        header: t`Title`,
+        accessorFn: play => {
+          const settings = play.settings?.map((s) => s.description).join(' ');
+          let text = `${play.title} ${play.subtitle} ${settings}`;
+          play.keywords?.forEach(k => text += ` ${k}`);
+          play.cast?.forEach(c => text += ` ${c.name}`)
+          return text;
+        },
+        cell: info => <TitleCell play={info.row.original} urlPath="/"/>,
+      },
+      {
+        id: 'normalizedYear',
+        header: t`Year (normalized)`,
+        accessorFn: row => row.normalizedYear?.toString() || '',
+      },
+      {
+        id: 'numberOfScenes',
+        header: t`Scenes`,
+        accessorFn: row => row.numberOfScenes?.toString() || '',
+      },
+      {
+        id: 'numberOfCharacters',
+        header: t`Characters`,
+        accessorFn: row => row.numberOfCharacters?.toString() || '',
+      },
+      {
+        id: 'keywords',
+        header: t`Keywords`,
+        accessorFn: row => row.keywords?.join(' ') || '',
+        cell: info => info.row.original.keywords ? (
+          <KeywordsCell keywords={info.row.original.keywords}/>
+        ) : <span/>,
+      },
+    ],
+    []
+  );
 
-      if (!b) {
-        return order === 'asc' ? 1 : -1;
-      }
-
-      return order === 'asc' ? a - b : b - a;
-    }
-  }, {
-    dataField: 'numberOfScenes',
-    text: t`Scenes`,
-    sort: true
-  }, {  
-    dataField: 'numberOfCharacters',
-    text: t`Characters`,
-    sort: true
-  }, {
-    dataField: 'keywords',
-    text: t`Keywords`,
-    formatter: formatKeywords,
-    sort: false
-  }]
-  
-  const { SearchBar } = Search;
-  
   return (
     <>
       <Helmet>
         <title>Einakter</title>
       </Helmet>
-      <ToolkitProvider
-        keyField='slug'
-        data={data}
-        columns={columns}
-        search
-      >
-        {
-          props => (
-            <div className="p-4 overflow-x-auto">
-              <Statistics plays={data} authors={authors} className="mb-2 mt-2"/>
-              <span className="flex justify-between items-center">
-                <SearchBar className="m-0" { ...props.searchProps } placeholder={t`Search`}/>
-                <span>
-                  <a className="bg-primary px-2 py-1 ml-3 rounded-md text-neutral-100 drop-shadow-lg hover:drop-shadow-md hover:text-secondary-100 transition ease-in-out" href="data.json" download="einakter.json" title={t`Download JSON`}>
-                    JSON
-                  </a>
-                  <a className="bg-primary px-2 py-1 ml-3 rounded-md text-neutral-100 drop-shadow-lg hover:drop-shadow-md hover:text-secondary-100 transition ease-in-out" href="data.csv" download="einakter.csv" title={t`Download CSV`}>
-                    CSV
-                  </a>
-                </span>
-              </span>
-              <BootstrapTable
-                { ...props.baseProps }
-                bootstrap4
-                defaultSorted={[
-                  { dataField: 'normalizedYear', order: 'asc' }
-                ]}
-                defaultSortDirection="asc"
-              />
-            </div>
-          )
-        }
-      </ToolkitProvider>
+      <div className="p-4 overflow-x-auto">
+        <Statistics plays={data} authors={authors} className="mb-2 mt-2"/>
+        <span className="flex justify-between items-center">
+          <span>
+            <a className="bg-primary px-2 py-1 ml-3 rounded-md text-neutral-100 drop-shadow-lg hover:drop-shadow-md hover:text-secondary-100 transition ease-in-out" href="data.json" download="einakter.json" title={t`Download JSON`}>
+              JSON
+            </a>
+            <a className="bg-primary px-2 py-1 ml-3 rounded-md text-neutral-100 drop-shadow-lg hover:drop-shadow-md hover:text-secondary-100 transition ease-in-out" href="data.csv" download="einakter.csv" title={t`Download CSV`}>
+              CSV
+            </a>
+          </span>
+        </span>
+        <Table
+          columns={columns}
+          data={data}
+          defaultSort={[{id: 'normalizedYear', desc: false}]}
+        />
+      </div>
     </>
   );
 }
-
-export default Table;
---
+++ src/components/Table.tsx
@@ -0,0 +1,111 @@
+import React, {useState} from 'react';
+import {t} from '@lingui/macro';
+import {classnames} from 'tailwindcss-classnames';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  SortDirection,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table';
+import DebouncedInput from './DebouncedInput';
+import {Play, OriginalPlay} from '../types';
+
+
+interface Props {
+  data: any[];
+  columns: ColumnDef<OriginalPlay|Play>[];
+  defaultSort?: SortingState;
+};
+
+const Table = ({columns, data: initialData, defaultSort = []}: Props) => {
+  const [sorting, setSorting] = useState<SortingState>(defaultSort);
+  const [globalFilter, setGlobalFilter] = useState('');
+  const [data] = useState(initialData);
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+      globalFilter,
+    },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setGlobalFilter,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    debugTable: true,
+  });
+
+  return (
+    <>
+      <div className="mb-2">
+        <DebouncedInput
+          value={globalFilter ?? ''}
+          onChange={value => setGlobalFilter(String(value))}
+          placeholder={t`Search`}
+        />
+        {globalFilter !== '' && (
+          <span className="ml-2">({table.getRowModel().rows.length})</span>
+        )}
+      </div>
+      <div className="flex">
+        <table>
+          <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div
+                        {...{
+                          className: header.column.getCanSort()
+                            ? 'cursor-pointer select-none'
+                            : '',
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {' '}
+                        <SortIndicator status={header.column.getIsSorted()}/>
+                      </div>
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map(row => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Table;
+
+export function SortIndicator ({status}: {status: false | SortDirection}) {
+  return (
+    <>
+      <span className={classnames({'opacity-40': status !== 'asc'})}>▲</span>
+      <span className={classnames({'opacity-40': status !== 'desc'})}>▼</span>
+   </>
+  );
+}
