cc0602989bdf1c46fb3325fc435d7c38c8c5faf6
Carsten Milling <cmil@hashtable.de>
2022-11-24 09:43:45 +0100

    Fix typescript error

--- fetch-authors.ts
+++ fetch-authors.ts
@@ -29,7 +29,7 @@ let data: Play[] = [];
 try {
   data = loadAll(readFileSync('./data.yaml', 'utf8'), null, {
     schema: CORE_SCHEMA
-  });
+  }) as Play[];
 } catch (error) {
   console.log(error);
 }
