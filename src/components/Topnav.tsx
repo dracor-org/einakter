f741943a42d476cabcdb8d610d6b890fa4e5496e
Carsten Milling <carsten.milling@gmail.com>
2026-08-09 14:55:31 +0200

    Use dracor/react's LanguageMenu component

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,50 +1,22 @@
-import {Fragment, useContext} from 'react';
+import {useContext} from 'react';
 import {useLingui} from '@lingui/react';
 import {t} from '@lingui/core/macro';
-import {faLanguage} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Menu, Transition} from '@headlessui/react';
 import {useLocation} from '@tanstack/react-router';
-import {NavBar} from '@dracor/react';
+import {LanguageMenu, NavBar} from '@dracor/react';
 import {EinakterContext} from '../context';
 import {locales, setLocale} from '../i18n';
 import pkg from '../../package.json';
 
 const version = import.meta.env.VITE_VERSION || pkg.version;
 
-function LanguageMenu() {
+function EinakterLanguageMenu() {
   const {i18n} = useLingui();
   return (
-    <Menu>
-      <Menu.Button className="relative justify-center inline-flex mr-8 hover:text-neutral-100">
-        <FontAwesomeIcon icon={faLanguage} size="2x" />
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="flex flex-col origin-top absolute top-6 mt-2 p-2.5 gap-2 rounded-sm shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
-            {locales.map((locale) => (
-              <Menu.Item key={locale}>
-                {({active}) => (
-                  <span
-                    key={locale}
-                    className={`${locale === i18n.locale ? 'font-bold' : 'font-normal'} ${active ? 'text-blue-500' : 'text-blue-900'} uppercase hover:text-blue-700 focus:text-blue-400`}
-                    onClick={() => setLocale(locale)}
-                  >
-                    {locale}
-                  </span>
-                )}
-              </Menu.Item>
-            ))}
-          </Menu.Items>
-        </Transition>
-      </Menu.Button>
-    </Menu>
+    <LanguageMenu
+      languages={locales}
+      current={i18n.locale}
+      onSelect={setLocale}
+    />
   );
 }
 
@@ -76,7 +48,7 @@ export default function Topnav() {
         {label: t`Originals`, to: '/originals'},
         {label: t`About`, to: '/about'},
       ]}
-      addItem={<LanguageMenu />}
+      addItem={<EinakterLanguageMenu />}
     />
   );
 }
